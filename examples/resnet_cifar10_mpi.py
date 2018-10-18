7c2fccde2ad769b39c30ab54cc1bbb60989e42fe
Lie He <liam.he15@gmail.com>
2018-10-18 15:38:42 +0200

    Update function names.

--- examples/resnet_cifar10_mpi.py
+++ examples/resnet_cifar10_mpi.py
@@ -1,14 +1,14 @@
-import argparse
-import torch.distributed as dist
+import types
 
 
-from mlbench_core.models.pytorch import Models
-from mlbench_core.utils.pytorch.optim import Optimizer
-from mlbench_core.utils.pytorch.lr import Scheduler
-from mlbench_core.utils.pytorch.criterion import Criterion
-from mlbench_core.utils.pytorch.metrics import Metrics
-from mlbench_core.utils.pytorch.controlflow import ControlFlow
-from mlbench_core.utils.pytorch.utils import initialize
+from mlbench_core.models.pytorch import get_model
+from mlbench_core.optim.pytorch import get_optimizer
+from mlbench_core.lr_scheduler.pytorch import get_scheduler
+from mlbench_core.evaluation.pytorch import get_loss_function
+from mlbench_core.evaluation.pytorch import get_metrics
+from mlbench_core.controlflow.pytorch import get_controlflow
+from mlbench_core.utils.pytorch import initialize_backends
+
 from mlbench_core.dataset.imagerecognition.pytorch.dataloader import UniformPartitionedDataloader
 
 # Fixed for closed division
@@ -26,21 +26,24 @@ dataset_config = {
 model_config = {
     "model": "resnet20",
     "model_version": 2,
+    "average_models": True,  # Sum or average models between workers
 }
 
 utils_config = {
     "optim": "sgd",
     "nesterov": True,
-    "weight_decay": 0,
+    "weight_decay": 0.0001,
     "momentum": 0.9,
     "lr": 0.1,
-    "lr_scheduler": "const",
+    "lr_scheduler": "MultiStepLR",
+    "multisteplr_milestones": [82, 109],
+    "multisteplr_gamma": 0.1,
     "lr_scheduler_level": "epoch",
-    "criterion": "CrossEntropyLoss",
-    "metrics": "topk",
+    "loss_function": "CrossEntropyLoss",
+    "metrics": ["top1", "top5"],
 
     "train_epochs": 164,
-    "max_train_steps": 5,
+    "max_train_steps": 164,
     "max_batch_per_epoch": None,
     "resume": False,
     "runtime": {},
@@ -50,7 +53,7 @@ utils_config = {
     "transform_target_type": False,
     "validation": True,
     "seed": 42,
-    "repartition_per_epoch": True
+    "repartition_per_epoch": True,
 }
 
 # configurations which does not influence accuracy.
@@ -64,11 +67,10 @@ meta_config = {
 }
 
 
-config = argparse.Namespace(**meta_config, **dataset_config, **model_config, **utils_config)
+config = types.SimpleNamespace(**meta_config, **dataset_config, **model_config, **utils_config)
 
-# TODO: Update it to a more general one
 config.run_id = '1'
-initialize(config)
+initialize_backends(config)
 
 
 def generate_dataloader(config):
@@ -77,23 +79,22 @@ def generate_dataloader(config):
     return dataloader_train, dataloader_val
 
 
-# model = get_resnet_model(config)
-model = Models.create(config)
+model = get_model(config)
 
 # Create an optimizer associated with the model
-optimizer = Optimizer.create(config, model)
+optimizer = get_optimizer(config, model)
 
 # Create a learning rate scheduler for an optimizer
-scheduler = Scheduler.create(config, optimizer)
+scheduler = get_scheduler(config, optimizer)
 
-# A criterion for computing the loss
-criterion = Criterion.create(config, model)
+# A loss_function for computing the loss
+loss_function = get_loss_function(config, model)
 
 # Metrics like Top 1/5 Accuracy
-metrics = Metrics.create(config)
+metrics = [get_metrics(m) for m in config.metrics]
 
-controlflow = ControlFlow.create(config)
+controlflow = get_controlflow(config)
 
-controlflow(model=model, optimizer=optimizer, criterion=criterion,
-            metrics=metrics, scheduler=scheduler, options=config,
+controlflow(model=model, optimizer=optimizer, loss_function=loss_function,
+            metrics=metrics, scheduler=scheduler, config=config,
             dataloader_fn=generate_dataloader)
