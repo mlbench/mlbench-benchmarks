6bf3a87bce6738ca6848fdfbcbd8b5455daf2c47
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2019-03-14 15:18:56 +0100

    Updates pytorch models for new train validation code

--- pytorch/imagerecognition/openmpi-cifar10-resnet20-all-reduce/main.py
+++ pytorch/imagerecognition/openmpi-cifar10-resnet20-all-reduce/main.py
@@ -7,12 +7,13 @@ import argparse
 import json
 import os
 
-from mlbench_core.controlflow.pytorch import create_train_validation_step
+from mlbench_core.controlflow.pytorch import train_round, validation_round
 from mlbench_core.controlflow.pytorch.checkpoints_evaluation import CheckpointsEvaluationControlFlow
 from mlbench_core.dataset.imagerecognition.pytorch import CIFAR10V1, partition_dataset_by_rank
 from mlbench_core.evaluation.pytorch.metrics import TopKAccuracy
 from mlbench_core.models.pytorch.resnet import ResNetCIFAR
 from mlbench_core.optim.pytorch.optim import CentralizedSGD
+from mlbench_core.utils import Tracker
 from mlbench_core.utils.pytorch import initialize_backends
 from mlbench_core.utils.pytorch.checkpoint import CheckpointFreq
 from mlbench_core.utils.pytorch.checkpoint import Checkpointer
@@ -106,26 +107,22 @@ def main(run_id, dataset_dir, ckpt_run_dir, output_dir, validation_only=False,
         freq=CheckpointFreq.NONE)
 
     if not validation_only:
-        trainstep, validstep, tracker = create_train_validation_step(
-            model,
-            optimizer,
-            loss_function,
-            metrics,
-            scheduler,
-            batch_size,
-            rank,
-            run_id,
-            dtype='fp32',
-            schedule_per='epoch',
-            transform_target_type=None,
-            use_cuda=False,
-            max_batch_per_epoch=max_batch_per_epoch)
+        tracker = Tracker(metrics, run_id, rank)
 
         dist.barrier()
         for epoch in range(0, train_epochs):
-            trainstep(train_loader)
+            train_round(train_loader, model, optimizer, loss_function, metrics,
+                        scheduler, 'fp32', schedule_per='epoch',
+                        transform_target_type=None, use_cuda=use_cuda,
+                        max_batch_per_epoch=max_batch_per_epoch,
+                        tracker=tracker)
 
-            is_best = validstep(val_loader)
+            is_best = validation_round(val_loader, model,  loss_function,
+                                       metrics, run_id, rank, 'fp32',
+                                       transform_target_type=None,
+                                       use_cuda=use_cuda,
+                                       max_batch_per_epoch=max_batch_per_epoch,
+                                       tracker=tracker)
 
             checkpointer.save(tracker, model,
                               optimizer, scheduler,
