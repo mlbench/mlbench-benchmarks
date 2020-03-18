647f9102f33c9afb1c8d9ff70e5363dcbcb26a01
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-03-18 13:01:00 +0100

    Update main, requirements and Dockerfile

--- pytorch/translation/openmpi-wmt14-gnmt/main.py
+++ pytorch/translation/openmpi-wmt14-gnmt/main.py
@@ -16,28 +16,34 @@ import time
 
 import torch
 import torch.distributed as dist
-from mlbench_core.controlflow.pytorch.checkpoints_evaluation import \
-    CheckpointsEvaluationControlFlow
+import torchtext
+from mlbench_core.controlflow.pytorch.checkpoints_evaluation import (
+    CheckpointsEvaluationControlFlow,
+)
 from mlbench_core.controlflow.pytorch.gnmt import GNMTTrainer
-from mlbench_core.dataset.translation.pytorch.dataloader import WMT14Dataset
-from mlbench_core.evaluation.goals import task1_time_to_accuracy_goal, \
-    task1_time_to_accuracy_light_goal
+from mlbench_core.dataset.translation.pytorch import WMT14Dataset, config
+from mlbench_core.dataset.util.pytorch import partition_dataset_by_rank
 from mlbench_core.evaluation.pytorch.inference import Translator
 from mlbench_core.evaluation.pytorch.metrics import BLEUScore
 from mlbench_core.evaluation.pytorch.utils import build_criterion
-from mlbench_core.lr_scheduler.pytorch.lr import \
-    ExponentialWarmupMultiStepLR
 from mlbench_core.models.pytorch.gnmt import GNMT
 from mlbench_core.optim.pytorch.utils import build_fp_optimizer
 from mlbench_core.utils import Tracker
 from mlbench_core.utils.pytorch import initialize_backends
 from mlbench_core.utils.pytorch.checkpoint import CheckpointFreq, Checkpointer
 
-logger = logging.getLogger('mlbench')
+logger = logging.getLogger("mlbench")
 
 
-def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
-               validation_only=False, use_cuda=False, light_target=False):
+def train_loop(
+    run_id,
+    dataset_dir,
+    ckpt_run_dir,
+    output_dir,
+    validation_only=False,
+    use_cuda=False,
+    light_target=False,
+):
     """Train loop"""
     num_parallel_workers = 2
     max_batch_per_epoch = None
@@ -46,10 +52,11 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
     # Dataset attributes
     train_min_len, train_max_len = 0, 50
     val_min_len, val_max_len = 0, 125
-    math_mode = "fp32"
+    math_mode = "fp16"  # One of `fp16`, `fp32` or `manual_fp16`
     batch_first = False
     include_lengths = True
     max_size = None
+    lang = {"src": "en", "trg": "de"}
 
     # Model attributes
     hidden_size = 1024
@@ -64,145 +71,161 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
     val_batch_size = 64
     grad_clip = 5.0
 
+    # Translator
+    beam_size = 5
+    len_norm_factor = 0.6
+    cov_penalty_factor = 0.1
+    len_norm_const = 5.0
+    max_seq_len = 150
+
     # Optimizer
-    opt_config = {'optimizer': "Adam", 'lr': 2.00e-3}
+    opt_config = {"optimizer": "Adam", "lr": 2.00e-3}
 
     # Loss
-    loss_scaling = {
-        'init_scale': 8192,
-        'upscale_interval': 128
-    }
+    loss_scaling = {"init_scale": 8192, "upscale_interval": 128}
 
     # Scheduler
-    scheduler_config = {'warmup_steps': 200,
-                        'remain_steps': 0.666,
-                        'decay_interval': None,
-                        'decay_steps': 4,
-                        'decay_factor': 0.5}
+    scheduler_config = {
+        "warmup_steps": 200,
+        "remain_steps": 0.666,
+        "decay_interval": None,
+        "decay_steps": 4,
+        "decay_factor": 0.5,
+    }
 
     rank = dist.get_rank()
     world_size = dist.get_world_size()
 
     # Build train/val datsets
-    train_set = WMT14Dataset(dataset_dir,
-                             train=True,
-                             download=True,
-                             include_lengths=include_lengths,
-                             math_precision=math_mode,
-                             batch_first=batch_first,
-                             min_len=train_min_len,
-                             max_len=train_max_len,
-                             max_size=max_size)
+    train_set = WMT14Dataset(
+        dataset_dir,
+        batch_first=batch_first,
+        include_lengths=include_lengths,
+        math_precision=math_mode,
+        lang=lang,
+        train=True,
+        download=True,
+        lazy=True,
+        min_len=train_min_len,
+        max_len=train_max_len,
+    )
 
-    val_set = WMT14Dataset(dataset_dir,
-                           validation=True,
-                           download=False,
-                           include_lengths=include_lengths,
-                           math_precision=math_mode,
-                           batch_first=batch_first,
-                           min_len=val_min_len,
-                           max_len=val_max_len,
-                           max_size=max_size)
-
-    # train_set = partition_dataset_by_rank(train_set, rank, world_size)
-    # val_set = partition_dataset_by_rank(val_set, rank, world_size)
+    val_set = WMT14Dataset(
+        dataset_dir,
+        batch_first=batch_first,
+        include_lengths=include_lengths,
+        math_precision=math_mode,
+        lang=lang,
+        validation=True,
+        download=False,
+        min_len=val_min_len,
+        max_len=val_max_len,
+    )
 
     # Build model
-    model = GNMT(vocab_size=train_set.vocab_size,
-                 padding_idx=train_set.get_special_token_idx("PAD"),
-                 hidden_size=hidden_size,
-                 num_layers=num_layers,
-                 dropout=dropout,
-                 batch_first=batch_first,
-                 share_embedding=share_embedding,
-                 )
-
-    # Build optimizer
-    fp_optimizer, optimizer, model = build_fp_optimizer(model=model, math=math_mode,
-                                                        opt_config=opt_config, grad_clip=grad_clip, loss_scaling=loss_scaling)
+    model = GNMT(
+        vocab_size=train_set.vocab_size,
+        hidden_size=hidden_size,
+        num_layers=num_layers,
+        dropout=dropout,
+        batch_first=batch_first,
+        share_embedding=share_embedding,
+    )
 
     # Build loss function
-    loss_function = build_criterion(train_set.get_special_token_idx("PAD"), smoothing)
-
-    total_train_iters = len(train_set) // train_iter_size * train_epochs
-
-    # Create a learning rate scheduler for an optimizer
-    scheduler = ExponentialWarmupMultiStepLR(optimizer,
-                                             total_train_iters,
-                                             **scheduler_config)
-
-    # Translator
-    translator_config = {
-        "model": model,
-        "trg_tokenizer": train_set.fields['trg'],
-        "BOS_idx": train_set.get_special_token_idx("BOS"),
-        "EOS_idx": train_set.get_special_token_idx("EOS"),
-    }
-    translator = Translator(**translator_config)
+    loss_function = build_criterion(config.PAD, smoothing)
 
     if use_cuda:
         model = model.cuda()
         loss_function = loss_function.cuda()
 
+    # Translator
+    translator = Translator(
+        model=model,
+        trg_tokenizer=train_set.fields["trg"],
+        beam_size=beam_size,
+        len_norm_factor=len_norm_factor,
+        len_norm_const=len_norm_const,
+        cov_penalty_factor=cov_penalty_factor,
+        max_seq_len=max_seq_len,
+    )
+
     # Metrics like Top 1/5 Accuracy
     metrics = [BLEUScore()]
 
-    train_loader = train_set.get_loader(batch_size=train_batch_size,
-                                        shuffle=False,
-                                        device=torch.device(
-                                            'cuda' if use_cuda else 'cpu'))
-    val_loader = val_set.get_loader(batch_size=val_batch_size,
-                                    shuffle=False,
-                                    device=torch.device(
-                                        'cuda' if use_cuda else 'cpu'))
+    # Partition data
+    train_set = partition_dataset_by_rank(train_set, rank, world_size)
+    val_set = partition_dataset_by_rank(val_set, rank, world_size)
 
-    trainer = GNMTTrainer(model=model,
-                          criterion=loss_function,
-                          fp_optimizer=fp_optimizer,
-                          scheduler=scheduler,
-                          translator=translator,
-                          rank=rank,
-                          schedule_per='batch',
-                          tracker=None,
-                          metrics=metrics,
-                          iter_size=train_iter_size)
-    checkpointer = Checkpointer(
-        ckpt_run_dir=ckpt_run_dir,
+    # Get data loaders
+    train_loader = torchtext.data.BucketIterator(
+        dataset=train_set.data,
+        batch_size=train_batch_size,
+        shuffle=False,
+        sort_within_batch=True,
+        device=torch.device("cuda" if use_cuda else "cpu"),
+        sort_key=lambda x: len(x.src),
+    )
+
+    val_loader = torchtext.data.BucketIterator(
+        dataset=val_set.data,
+        batch_size=val_batch_size,
+        shuffle=False,
+        sort_within_batch=True,
+        device=torch.device("cuda" if use_cuda else "cpu"),
+        sort_key=lambda x: len(x.src),
+    )
+
+    # Build optimizer & scheduler
+    total_train_iters = (len(train_loader) // train_iter_size) * train_epochs
+    fp_optimizer, scheduler, model = build_fp_optimizer(
+        model=model,
+        math=math_mode,
+        opt_config=opt_config,
+        grad_clip=grad_clip,
+        loss_scaling=loss_scaling,
+        scheduler_config=scheduler_config,
+        iters=total_train_iters,
+    )
+
+    # Trainer
+    trainer = GNMTTrainer(
+        model=model,
+        criterion=loss_function,
+        fp_optimizer=fp_optimizer,
+        scheduler=scheduler,
+        translator=translator,
         rank=rank,
-        freq=CheckpointFreq.NONE)
+        schedule_per="batch",
+        tracker=None,
+        metrics=metrics,
+        iter_size=train_iter_size,
+    )
+    checkpointer = Checkpointer(
+        ckpt_run_dir=ckpt_run_dir, rank=rank, freq=CheckpointFreq.BEST
+    )
 
     if not validation_only:
-        if light_target:
-            goal = task1_time_to_accuracy_light_goal()
-        else:
-            goal = task1_time_to_accuracy_goal()
 
-        tracker = Tracker(metrics, run_id, rank, goal=goal)
+        # TODO Add goal
+        tracker = Tracker(metrics, run_id, rank, goal=None)
 
         trainer.set_tracker(tracker)
         dist.barrier()
-
         tracker.start()
 
         for epoch in range(0, train_epochs):
             trainer.train_round(train_loader)
-            # train_round(train_loader, model, optimizer, loss_function, metrics,
-            #             scheduler, 'fp32', schedule_per='epoch',
-            #             transform_target_type=None, use_cuda=use_cuda,
-            #             max_batch_per_epoch=max_batch_per_epoch,
-            #             tracker=tracker)
-
-            # is_best = validation_round(val_loader, model, loss_function,
-            #                            metrics, run_id, rank, 'fp32',
-            #                            transform_target_type=None,
-            #                            use_cuda=use_cuda,
-            #                            max_batch_per_epoch=max_batch_per_epoch,
-            #                            tracker=tracker)
 
             is_best = trainer.validation_round(val_loader)
-            checkpointer.save(tracker, model,
-                              optimizer, scheduler,
-                              tracker.current_epoch, is_best)
+            checkpointer.save(
+                tracker,
+                model,
+                fp_optimizer.optimizer,
+                scheduler,
+                tracker.current_epoch,
+                is_best,
+            )
 
             tracker.epoch_end()
 
@@ -221,61 +244,103 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
             loss_function=loss_function,
             metrics=metrics,
             use_cuda=use_cuda,
-            dtype='fp32',
-            max_batch_per_epoch=None)
+            dtype="fp32",
+            max_batch_per_epoch=None,
+        )
 
         train_stats = cecf.evaluate_by_epochs(train_loader)
-        with open(os.path.join(output_dir, "train_stats.json"), 'w') as f:
+        with open(os.path.join(output_dir, "train_stats.json"), "w") as f:
             json.dump(train_stats, f)
 
         val_stats = cecf.evaluate_by_epochs(val_loader)
-        with open(os.path.join(output_dir, "val_stats.json"), 'w') as f:
+        with open(os.path.join(output_dir, "val_stats.json"), "w") as f:
             json.dump(val_stats, f)
 
 
-def main(run_id, dataset_dir, ckpt_run_dir, output_dir, validation_only=False,
-         gpu=False, light_target=False):
+def main(
+    run_id,
+    dataset_dir,
+    ckpt_run_dir,
+    output_dir,
+    validation_only=False,
+    gpu=False,
+    light_target=False,
+):
     r"""Main logic."""
 
     with initialize_backends(
-            comm_backend='mpi',
-            logging_level='INFO',
-            logging_file=os.path.join(output_dir, 'mlbench.log'),
+        comm_backend="mpi",
+        logging_level="INFO",
+        logging_file=os.path.join(output_dir, "mlbench.log"),
+        use_cuda=gpu,
+        seed=42,
+        cudnn_deterministic=False,
+        ckpt_run_dir=ckpt_run_dir,
+        delete_existing_ckpts=not validation_only,
+    ):
+        train_loop(
+            run_id,
+            dataset_dir,
+            ckpt_run_dir,
+            output_dir,
+            validation_only,
             use_cuda=gpu,
-            seed=42,
-            cudnn_deterministic=False,
-            ckpt_run_dir=ckpt_run_dir,
-            delete_existing_ckpts=not validation_only):
-        train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
-                   validation_only, use_cuda=gpu, light_target=light_target)
+            light_target=light_target,
+        )
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process run parameters')
-    parser.add_argument('--run_id', type=str, default='1',
-                        help='The id of the run')
-    parser.add_argument('--root-dataset', type=str, default='/datasets',
-                        help='Default root directory to dataset.')
-    parser.add_argument('--root-checkpoint', type=str, default='/checkpoint',
-                        help='Default root directory to checkpoint.')
-    parser.add_argument('--root-output', type=str, default='/output',
-                        help='Default root directory to output.')
-    parser.add_argument('--validation_only', action='store_true',
-                        default=False, help='Only validate from checkpoints.')
-    parser.add_argument('--gpu', action='store_true', default=False,
-                        help='Train with GPU')
-    parser.add_argument('--light', action='store_true', default=False,
-                        help='Train to light target metric goal')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process run parameters")
+    parser.add_argument("--run_id", type=str, default="1", help="The id of the run")
+    parser.add_argument(
+        "--root-dataset",
+        type=str,
+        default="/datasets",
+        help="Default root directory to dataset.",
+    )
+    parser.add_argument(
+        "--root-checkpoint",
+        type=str,
+        default="/checkpoint",
+        help="Default root directory to checkpoint.",
+    )
+    parser.add_argument(
+        "--root-output",
+        type=str,
+        default="/output",
+        help="Default root directory to output.",
+    )
+    parser.add_argument(
+        "--validation_only",
+        action="store_true",
+        default=False,
+        help="Only validate from checkpoints.",
+    )
+    parser.add_argument(
+        "--gpu", action="store_true", default=False, help="Train with GPU"
+    )
+    parser.add_argument(
+        "--light",
+        action="store_true",
+        default=False,
+        help="Train to light target metric goal",
+    )
     args = parser.parse_args()
 
-    uid = 'scaling'
-    dataset_dir = os.path.join(args.root_dataset, 'torch', 'wmt14')
+    uid = "scaling"
+    dataset_dir = os.path.join(args.root_dataset, "torch", "wmt14")
     ckpt_run_dir = os.path.join(args.root_checkpoint, uid)
     output_dir = os.path.join(args.root_output, uid)
     os.makedirs(dataset_dir, exist_ok=True)
     os.makedirs(ckpt_run_dir, exist_ok=True)
     os.makedirs(output_dir, exist_ok=True)
 
-    main(args.run_id, dataset_dir, ckpt_run_dir,
-         output_dir, validation_only=args.validation_only, gpu=args.gpu,
-         light_target=args.light)
+    main(
+        args.run_id,
+        dataset_dir,
+        ckpt_run_dir,
+        output_dir,
+        validation_only=args.validation_only,
+        gpu=args.gpu,
+        light_target=args.light,
+    )
