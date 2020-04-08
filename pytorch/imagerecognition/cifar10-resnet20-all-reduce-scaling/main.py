0cd3672352a66981e766cecb80e29cab192f269d
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-04-08 11:45:28 +0200

    Black formatting

--- pytorch/imagerecognition/cifar10-resnet20-all-reduce-scaling/main.py
+++ pytorch/imagerecognition/cifar10-resnet20-all-reduce-scaling/main.py
@@ -15,15 +15,17 @@ import time
 
 import torch.distributed as dist
 from mlbench_core.controlflow.pytorch import train_round, validation_round
-from mlbench_core.controlflow.pytorch.checkpoints_evaluation import \
-    CheckpointsEvaluationControlFlow
+from mlbench_core.controlflow.pytorch.checkpoints_evaluation import (
+    CheckpointsEvaluationControlFlow,
+)
 from mlbench_core.dataset.imagerecognition.pytorch import CIFAR10V1
 from mlbench_core.dataset.util.pytorch import partition_dataset_by_rank
-from mlbench_core.evaluation.goals import task1_time_to_accuracy_goal, \
-    task1_time_to_accuracy_light_goal
+from mlbench_core.evaluation.goals import (
+    task1_time_to_accuracy_goal,
+    task1_time_to_accuracy_light_goal,
+)
 from mlbench_core.evaluation.pytorch.metrics import TopKAccuracy
-from mlbench_core.lr_scheduler.pytorch.lr import \
-    MultistepLearningRatesWithWarmup
+from mlbench_core.lr_scheduler.pytorch.lr import MultistepLearningRatesWithWarmup
 from mlbench_core.models.pytorch.resnet import ResNetCIFAR
 from mlbench_core.optim.pytorch.optim import CentralizedSGD
 from mlbench_core.utils import Tracker
@@ -34,8 +36,15 @@ from torch.nn.modules.loss import CrossEntropyLoss
 from torch.utils.data import DataLoader
 
 
-def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
-               validation_only=False, use_cuda=False, light_target=False):
+def train_loop(
+    run_id,
+    dataset_dir,
+    ckpt_run_dir,
+    output_dir,
+    validation_only=False,
+    use_cuda=False,
+    light_target=False,
+):
     """Train loop"""
     num_parallel_workers = 2
     max_batch_per_epoch = None
@@ -45,11 +54,7 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
     rank = dist.get_rank()
     world_size = dist.get_world_size()
 
-    model = ResNetCIFAR(
-        resnet_size=20,
-        bottleneck=False,
-        num_classes=10,
-        version=1)
+    model = ResNetCIFAR(resnet_size=20, bottleneck=False, num_classes=10, version=1)
 
     optimizer = CentralizedSGD(
         world_size=world_size,
@@ -57,7 +62,8 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         lr=0.2,
         momentum=0.9,
         weight_decay=1e-4,
-        nesterov=False)
+        nesterov=False,
+    )
 
     # Create a learning rate scheduler for an optimizer
     scheduler = MultistepLearningRatesWithWarmup(
@@ -68,7 +74,8 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         lr=0.1,
         warmup_duration=5,
         warmup_linear_scaling=True,
-        warmup_init_lr=None)
+        warmup_init_lr=None,
+    )
 
     # A loss_function for computing the loss
     loss_function = CrossEntropyLoss()
@@ -78,10 +85,7 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         loss_function = loss_function.cuda()
 
     # Metrics like Top 1/5 Accuracy
-    metrics = [
-        TopKAccuracy(topk=1),
-        TopKAccuracy(topk=5)
-    ]
+    metrics = [TopKAccuracy(topk=1), TopKAccuracy(topk=5)]
 
     train_set = CIFAR10V1(dataset_dir, train=True, download=True)
     val_set = CIFAR10V1(dataset_dir, train=False, download=True)
@@ -95,7 +99,8 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         shuffle=True,
         num_workers=num_parallel_workers,
         pin_memory=use_cuda,
-        drop_last=False)
+        drop_last=False,
+    )
 
     val_loader = DataLoader(
         val_set,
@@ -103,12 +108,12 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         shuffle=False,
         num_workers=num_parallel_workers,
         pin_memory=use_cuda,
-        drop_last=False)
+        drop_last=False,
+    )
 
     checkpointer = Checkpointer(
-        ckpt_run_dir=ckpt_run_dir,
-        rank=rank,
-        freq=CheckpointFreq.NONE)
+        ckpt_run_dir=ckpt_run_dir, rank=rank, freq=CheckpointFreq.NONE
+    )
 
     if not validation_only:
         if light_target:
@@ -123,22 +128,38 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         tracker.start()
 
         for epoch in range(0, train_epochs):
-            train_round(train_loader, model, optimizer, loss_function, metrics,
-                        scheduler, 'fp32', schedule_per='epoch',
-                        transform_target_type=None, use_cuda=use_cuda,
-                        max_batch_per_epoch=max_batch_per_epoch,
-                        tracker=tracker)
+            train_round(
+                train_loader,
+                model,
+                optimizer,
+                loss_function,
+                metrics,
+                scheduler,
+                "fp32",
+                schedule_per="epoch",
+                transform_target_type=None,
+                use_cuda=use_cuda,
+                max_batch_per_epoch=max_batch_per_epoch,
+                tracker=tracker,
+            )
 
-            is_best = validation_round(val_loader, model, loss_function,
-                                       metrics, run_id, rank, 'fp32',
-                                       transform_target_type=None,
-                                       use_cuda=use_cuda,
-                                       max_batch_per_epoch=max_batch_per_epoch,
-                                       tracker=tracker)
+            is_best = validation_round(
+                val_loader,
+                model,
+                loss_function,
+                metrics,
+                run_id,
+                rank,
+                "fp32",
+                transform_target_type=None,
+                use_cuda=use_cuda,
+                max_batch_per_epoch=max_batch_per_epoch,
+                tracker=tracker,
+            )
 
-            checkpointer.save(tracker, model,
-                              optimizer, scheduler,
-                              tracker.current_epoch, is_best)
+            checkpointer.save(
+                tracker, model, optimizer, scheduler, tracker.current_epoch, is_best
+            )
 
             tracker.epoch_end()
 
@@ -157,66 +178,114 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
             loss_function=loss_function,
             metrics=metrics,
             use_cuda=use_cuda,
-            dtype='fp32',
-            max_batch_per_epoch=None)
+            dtype="fp32",
+            max_batch_per_epoch=None,
+        )
 
         train_stats = cecf.evaluate_by_epochs(train_loader)
-        with open(os.path.join(output_dir, "train_stats.json"), 'w') as f:
+        with open(os.path.join(output_dir, "train_stats.json"), "w") as f:
             json.dump(train_stats, f)
 
         val_stats = cecf.evaluate_by_epochs(val_loader)
-        with open(os.path.join(output_dir, "val_stats.json"), 'w') as f:
+        with open(os.path.join(output_dir, "val_stats.json"), "w") as f:
             json.dump(val_stats, f)
 
 
-def main(run_id, dataset_dir, ckpt_run_dir, output_dir, rank, hosts, backend, validation_only=False,
-         gpu=False, light_target=False):
+def main(
+    run_id,
+    dataset_dir,
+    ckpt_run_dir,
+    output_dir,
+    rank,
+    hosts,
+    backend,
+    validation_only=False,
+    gpu=False,
+    light_target=False,
+):
     r"""Main logic."""
 
     with initialize_backends(
-            comm_backend='mpi',
-            logging_level='INFO',
-            logging_file=os.path.join(output_dir, 'mlbench.log'),
+        comm_backend="mpi",
+        logging_level="INFO",
+        logging_file=os.path.join(output_dir, "mlbench.log"),
+        use_cuda=gpu,
+        seed=42,
+        cudnn_deterministic=False,
+        ckpt_run_dir=ckpt_run_dir,
+        delete_existing_ckpts=not validation_only,
+    ):
+        train_loop(
+            run_id,
+            dataset_dir,
+            ckpt_run_dir,
+            output_dir,
+            validation_only,
             use_cuda=gpu,
-            seed=42,
-            cudnn_deterministic=False,
-            ckpt_run_dir=ckpt_run_dir,
-            delete_existing_ckpts=not validation_only):
-        train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
-                   validation_only, use_cuda=gpu, light_target=light_target)
+            light_target=light_target,
+        )
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process run parameters')
-    parser.add_argument('--run_id', type=str, default='1',
-                        help='The id of the run')
-    parser.add_argument('--root-dataset', type=str, default='/datasets',
-                        help='Default root directory to dataset.')
-    parser.add_argument('--root-checkpoint', type=str, default='/checkpoint',
-                        help='Default root directory to checkpoint.')
-    parser.add_argument('--root-output', type=str, default='/output',
-                        help='Default root directory to output.')
-    parser.add_argument('--validation_only', action='store_true',
-                        default=False, help='Only validate from checkpoints.')
-    parser.add_argument('--gpu', action='store_true', default=False,
-                        help='Train with GPU')
-    parser.add_argument('--light', action='store_true', default=False,
-                        help='Train to light target metric goal')
-    parser.add_argument('--rank', type=int, default=1,
-                        help='The rank of the process')
-    parser.add_argument('--backend', type=str, default='mpi',
-                        help='PyTorch distributed backend')
-    parser.add_argument('--hosts', type=str, help='The list of hosts')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process run parameters")
+    parser.add_argument("--run_id", type=str, default="1", help="The id of the run")
+    parser.add_argument(
+        "--root-dataset",
+        type=str,
+        default="/datasets",
+        help="Default root directory to dataset.",
+    )
+    parser.add_argument(
+        "--root-checkpoint",
+        type=str,
+        default="/checkpoint",
+        help="Default root directory to checkpoint.",
+    )
+    parser.add_argument(
+        "--root-output",
+        type=str,
+        default="/output",
+        help="Default root directory to output.",
+    )
+    parser.add_argument(
+        "--validation_only",
+        action="store_true",
+        default=False,
+        help="Only validate from checkpoints.",
+    )
+    parser.add_argument(
+        "--gpu", action="store_true", default=False, help="Train with GPU"
+    )
+    parser.add_argument(
+        "--light",
+        action="store_true",
+        default=False,
+        help="Train to light target metric goal",
+    )
+    parser.add_argument("--rank", type=int, default=1, help="The rank of the process")
+    parser.add_argument(
+        "--backend", type=str, default="mpi", help="PyTorch distributed backend"
+    )
+    parser.add_argument("--hosts", type=str, help="The list of hosts")
     args = parser.parse_args()
 
-    uid = 'scaling'
-    dataset_dir = os.path.join(args.root_dataset, 'torch', 'cifar10')
+    uid = "scaling"
+    dataset_dir = os.path.join(args.root_dataset, "torch", "cifar10")
     ckpt_run_dir = os.path.join(args.root_checkpoint, uid)
     output_dir = os.path.join(args.root_output, uid)
     os.makedirs(dataset_dir, exist_ok=True)
     os.makedirs(ckpt_run_dir, exist_ok=True)
     os.makedirs(output_dir, exist_ok=True)
 
-    main(args.run_id, dataset_dir, ckpt_run_dir,
-         output_dir, args.rank, args.hosts, args.backend, validation_only=args.validation_only, gpu=args.gpu,
-         light_target=args.light)
+    main(
+        args.run_id,
+        dataset_dir,
+        ckpt_run_dir,
+        output_dir,
+        args.rank,
+        args.hosts,
+        args.backend,
+        validation_only=args.validation_only,
+        gpu=args.gpu,
+        light_target=args.light,
+    )
--- pytorch/linearmodels/epsilon-logistic-regression-all-reduce/main.py
+++ pytorch/linearmodels/epsilon-logistic-regression-all-reduce/main.py
@@ -13,14 +13,21 @@ import time
 
 import torch.distributed as dist
 from mlbench_core.controlflow.pytorch import train_round, validation_round
-from mlbench_core.controlflow.pytorch.checkpoints_evaluation import \
-    CheckpointsEvaluationControlFlow
+from mlbench_core.controlflow.pytorch.checkpoints_evaluation import (
+    CheckpointsEvaluationControlFlow,
+)
 from mlbench_core.dataset.linearmodels.pytorch.dataloader import LMDBDataset
 from mlbench_core.dataset.util.pytorch import partition_dataset_by_rank
-from mlbench_core.evaluation.goals import task2_time_to_accuracy_light_goal, task2_time_to_accuracy_goal
+from mlbench_core.evaluation.goals import (
+    task2_time_to_accuracy_light_goal,
+    task2_time_to_accuracy_goal,
+)
 from mlbench_core.evaluation.pytorch.criterion import BCELossRegularized
-from mlbench_core.evaluation.pytorch.metrics import F1Score, DiceCoefficient, \
-    TopKAccuracy
+from mlbench_core.evaluation.pytorch.metrics import (
+    F1Score,
+    DiceCoefficient,
+    TopKAccuracy,
+)
 from mlbench_core.lr_scheduler.pytorch.lr import SQRTTimeDecayLR
 from mlbench_core.models.pytorch.linear_models import LogisticRegression
 from mlbench_core.optim.pytorch.optim import CentralizedSGD
@@ -31,8 +38,15 @@ from mlbench_core.utils.pytorch.checkpoint import Checkpointer
 from torch.utils.data import DataLoader
 
 
-def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
-               validation_only=False, use_cuda=False, light_target=False):
+def train_loop(
+    run_id,
+    dataset_dir,
+    ckpt_run_dir,
+    output_dir,
+    validation_only=False,
+    use_cuda=False,
+    light_target=False,
+):
     r"""Main logic."""
     num_parallel_workers = 0
     max_batch_per_epoch = None
@@ -61,14 +75,14 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         model = model.cuda()
         loss_function = loss_function.cuda()
 
-    metrics = [TopKAccuracy(),  # Binary accuracy with threshold 0.5
-               F1Score(),
-               DiceCoefficient()]
+    metrics = [
+        TopKAccuracy(),  # Binary accuracy with threshold 0.5
+        F1Score(),
+        DiceCoefficient(),
+    ]
 
-    train_set = LMDBDataset(name="epsilon", data_type="train",
-                            root=dataset_dir)
-    val_set = LMDBDataset(name="epsilon", data_type="test",
-                          root=dataset_dir)
+    train_set = LMDBDataset(name="epsilon", data_type="train", root=dataset_dir)
+    val_set = LMDBDataset(name="epsilon", data_type="test", root=dataset_dir)
 
     train_set = partition_dataset_by_rank(train_set, rank, world_size)
     val_set = partition_dataset_by_rank(val_set, rank, world_size)
@@ -79,7 +93,8 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         shuffle=True,
         num_workers=num_parallel_workers,
         pin_memory=use_cuda,
-        drop_last=False)
+        drop_last=False,
+    )
 
     val_loader = DataLoader(
         val_set,
@@ -87,12 +102,12 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         shuffle=False,
         num_workers=num_parallel_workers,
         pin_memory=use_cuda,
-        drop_last=False)
+        drop_last=False,
+    )
 
     checkpointer = Checkpointer(
-        ckpt_run_dir=ckpt_run_dir,
-        rank=rank,
-        freq=CheckpointFreq.NONE)
+        ckpt_run_dir=ckpt_run_dir, rank=rank, freq=CheckpointFreq.NONE
+    )
 
     if not validation_only:
         if light_target:
@@ -107,22 +122,38 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
         tracker.start()
 
         for epoch in range(0, train_epochs):
-            train_round(train_loader, model, optimizer, loss_function, metrics,
-                        scheduler, 'fp32', schedule_per='epoch',
-                        transform_target_type=None, use_cuda=use_cuda,
-                        max_batch_per_epoch=max_batch_per_epoch,
-                        tracker=tracker)
+            train_round(
+                train_loader,
+                model,
+                optimizer,
+                loss_function,
+                metrics,
+                scheduler,
+                "fp32",
+                schedule_per="epoch",
+                transform_target_type=None,
+                use_cuda=use_cuda,
+                max_batch_per_epoch=max_batch_per_epoch,
+                tracker=tracker,
+            )
 
-            is_best = validation_round(val_loader, model, loss_function,
-                                       metrics, run_id, rank, 'fp32',
-                                       transform_target_type=None,
-                                       use_cuda=use_cuda,
-                                       max_batch_per_epoch=max_batch_per_epoch,
-                                       tracker=tracker)
+            is_best = validation_round(
+                val_loader,
+                model,
+                loss_function,
+                metrics,
+                run_id,
+                rank,
+                "fp32",
+                transform_target_type=None,
+                use_cuda=use_cuda,
+                max_batch_per_epoch=max_batch_per_epoch,
+                tracker=tracker,
+            )
 
-            checkpointer.save(tracker, model,
-                              optimizer, scheduler,
-                              tracker.current_epoch, is_best)
+            checkpointer.save(
+                tracker, model, optimizer, scheduler, tracker.current_epoch, is_best
+            )
 
             tracker.epoch_end()
 
@@ -141,68 +172,117 @@ def train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
             loss_function=loss_function,
             metrics=metrics,
             use_cuda=use_cuda,
-            dtype='fp32',
-            max_batch_per_epoch=None)
+            dtype="fp32",
+            max_batch_per_epoch=None,
+        )
 
         train_stats = cecf.evaluate_by_epochs(train_loader)
-        with open(os.path.join(output_dir, "train_stats.json"), 'w') as f:
+        with open(os.path.join(output_dir, "train_stats.json"), "w") as f:
             json.dump(train_stats, f)
 
         val_stats = cecf.evaluate_by_epochs(val_loader)
-        with open(os.path.join(output_dir, "val_stats.json"), 'w') as f:
+        with open(os.path.join(output_dir, "val_stats.json"), "w") as f:
             json.dump(val_stats, f)
 
 
-def main(run_id, dataset_dir, ckpt_run_dir, output_dir, rank, backend, hosts, validation_only=False,
-         gpu=False, light_target=False):
+def main(
+    run_id,
+    dataset_dir,
+    ckpt_run_dir,
+    output_dir,
+    rank,
+    backend,
+    hosts,
+    validation_only=False,
+    gpu=False,
+    light_target=False,
+):
     r"""Main logic."""
 
     with initialize_backends(
-            comm_backend=backend,
-            hosts=hosts,
-            rank=rank,
-            logging_level='INFO',
-            logging_file=os.path.join(output_dir, 'mlbench.log'),
+        comm_backend=backend,
+        hosts=hosts,
+        rank=rank,
+        logging_level="INFO",
+        logging_file=os.path.join(output_dir, "mlbench.log"),
+        use_cuda=gpu,
+        seed=42,
+        cudnn_deterministic=False,
+        ckpt_run_dir=ckpt_run_dir,
+        delete_existing_ckpts=not validation_only,
+    ):
+        train_loop(
+            run_id,
+            dataset_dir,
+            ckpt_run_dir,
+            output_dir,
+            validation_only,
             use_cuda=gpu,
-            seed=42,
-            cudnn_deterministic=False,
-            ckpt_run_dir=ckpt_run_dir,
-            delete_existing_ckpts=not validation_only):
-        train_loop(run_id, dataset_dir, ckpt_run_dir, output_dir,
-                    validation_only, use_cuda=gpu, light_target=light_target)
+            light_target=light_target,
+        )
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process run parameters')
-    parser.add_argument('--run_id', type=str, default='1',
-                        help='The id of the run')
-    parser.add_argument('--root-dataset', type=str, default='/datasets',
-                        help='Default root directory to dataset.')
-    parser.add_argument('--root-checkpoint', type=str, default='/checkpoint',
-                        help='Default root directory to checkpoint.')
-    parser.add_argument('--root-output', type=str, default='/output',
-                        help='Default root directory to output.')
-    parser.add_argument('--validation_only', action='store_true',
-                        default=False, help='Only validate from checkpoints.')
-    parser.add_argument('--gpu', action='store_true', default=False,
-                        help='Train with GPU')
-    parser.add_argument('--light', action='store_true', default=False,
-                        help='Train to light target metric goal')
-    parser.add_argument('--rank', type=int, default=1,
-                        help='The rank of the process')
-    parser.add_argument('--backend', type=str, default='mpi',
-                        help='PyTorch distributed backend')
-    parser.add_argument('--hosts', type=str, help='The list of hosts')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process run parameters")
+    parser.add_argument("--run_id", type=str, default="1", help="The id of the run")
+    parser.add_argument(
+        "--root-dataset",
+        type=str,
+        default="/datasets",
+        help="Default root directory to dataset.",
+    )
+    parser.add_argument(
+        "--root-checkpoint",
+        type=str,
+        default="/checkpoint",
+        help="Default root directory to checkpoint.",
+    )
+    parser.add_argument(
+        "--root-output",
+        type=str,
+        default="/output",
+        help="Default root directory to output.",
+    )
+    parser.add_argument(
+        "--validation_only",
+        action="store_true",
+        default=False,
+        help="Only validate from checkpoints.",
+    )
+    parser.add_argument(
+        "--gpu", action="store_true", default=False, help="Train with GPU"
+    )
+    parser.add_argument(
+        "--light",
+        action="store_true",
+        default=False,
+        help="Train to light target metric goal",
+    )
+    parser.add_argument("--rank", type=int, default=1, help="The rank of the process")
+    parser.add_argument(
+        "--backend", type=str, default="mpi", help="PyTorch distributed backend"
+    )
+    parser.add_argument("--hosts", type=str, help="The list of hosts")
 
     args = parser.parse_args()
 
-    uid = 'benchmark'
-    dataset_dir = os.path.join(args.root_dataset, 'torch', 'epsilon')
+    uid = "benchmark"
+    dataset_dir = os.path.join(args.root_dataset, "torch", "epsilon")
     ckpt_run_dir = os.path.join(args.root_checkpoint, uid)
     output_dir = os.path.join(args.root_output, uid)
     os.makedirs(dataset_dir, exist_ok=True)
     os.makedirs(ckpt_run_dir, exist_ok=True)
     os.makedirs(output_dir, exist_ok=True)
 
-    main(args.run_id, dataset_dir, ckpt_run_dir, output_dir, args.rank, args.backend, args.hosts,
-         validation_only=args.validation_only, gpu=args.gpu, light_target=args.light)
+    main(
+        args.run_id,
+        dataset_dir,
+        ckpt_run_dir,
+        output_dir,
+        args.rank,
+        args.backend,
+        args.hosts,
+        validation_only=args.validation_only,
+        gpu=args.gpu,
+        light_target=args.light,
+    )
