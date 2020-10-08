3fe1fb9ac61db6a78802468c93a05d1cb89f0e30
Edoardo Holzl <edoardo.holzl@epfl.ch>
2020-10-08 12:19:48 +0200

    Change 'Benchmarks' to 'Benchmark Implementations'

--- docs/gensidebar.py
+++ docs/gensidebar.py
@@ -76,17 +76,17 @@ def generate_sidebar(conf, conf_api):
     #
 
     toctree("MLBench")
+    write("Benchmarks", "benchmark-tasks")
     write("Prerequisites", "prerequisites")
     write("Installation", "installation")
     write("Component Overview", "overview")
-    write("Benchmarking Tasks", "benchmark-tasks")
     write("Tutorials", "tutorials")
     endl()
 
     toctree("Components")
     write_ref("mlbench_helm", "Helm Chart")
     write_ref("mlbench_dashboard", "Dashboard")
-    write_ref("mlbench_benchmarks", "Benchmarks")
+    write_ref("mlbench_benchmarks", "Benchmark Implementations")
     write_api("mlbench_core", "Core")
     endl()
 
